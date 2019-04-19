2cb7763d0422c3610c0264728d3c90deba46e732
songweida <505416496@qq.com>
2019-04-19 15:13:43 +0800

    编辑申报

---
+++ app/src/main/java/qx/app/freight/qxappfreight/activity/CollectorDeclareActivity.java
@@ -0,0 +1,20 @@
+package qx.app.freight.qxappfreight.activity;
+
+import android.os.Bundle;
+
+import qx.app.freight.qxappfreight.R;
+import qx.app.freight.qxappfreight.app.BaseActivity;
+
+public class CollectorDeclareActivity extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_collector_declare;
+    }
+
+    @Override
+    public void businessLogic(Bundle savedInstanceState) {
+
+    }
+
+}
