46dc6fa851e7de9a1bb0d545ed1903ce13a53ba2
pengrui <pengrui.com>
2019-04-23 18:59:15 +0800

    图片地址

--- app/src/main/java/qx/app/freight/qxappfreight/constant/HttpConstant.java
+++ app/src/main/java/qx/app/freight/qxappfreight/constant/HttpConstant.java
@@ -21,7 +21,7 @@ public class HttpConstant {
 //    public static final String WEBSOCKETURL = "ws://173.100.1.78:7004/socketServer?";
 //    public static final String WEBSOCKETURL = "ws://173.101.2.8:7004/socketServer?";//测试环境
 
-    public static final String IMAGEURL_Verify = "http://173.100.1.74/";
+    public static final String IMAGEURLVERIFY = "http://173.100.1.74/";
     public static final String IMAGEURL = "http://173.101.2.26/";//测试环境
 
 //    public static final String TEST = "http://173.101.2.8:80";
