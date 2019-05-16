e82359a603cd5cca67c79500095ebc26b2b1eda9
guohao <504575890@qq.com>
2019-05-16 16:57:41 +0800

    异常详情对话框,adapter修复

--- app/src/main/java/qx/app/freight/qxappfreight/dialog/ExceptionDetailDialog.java
+++ app/src/main/java/qx/app/freight/qxappfreight/dialog/ExceptionDetailDialog.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -84,6 +85,11 @@ public class ExceptionDetailDialog extends DialogFragment {
         Window window = dialog.getWindow();
         WindowManager.LayoutParams lp = window.getAttributes();
         lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+
+        MyAdapter adapter = new MyAdapter(inventoryDetailEntity.getInventoryUbnormalGoods(), context);
+        recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        recyclerView.setAdapter(adapter);
+
         return dialog;
     }
 
