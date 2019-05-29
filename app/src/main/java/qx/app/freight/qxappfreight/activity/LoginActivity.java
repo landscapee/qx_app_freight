2c6bb306c8318f03c146d305b3471e027d562d83
zzq <417986033@qq.com>
2019-05-29 11:57:59 +0800

    卸机小bug修复

--- app/src/main/java/qx/app/freight/qxappfreight/activity/LoginActivity.java
+++ app/src/main/java/qx/app/freight/qxappfreight/activity/LoginActivity.java
@@ -71,7 +71,7 @@ public class LoginActivity extends BaseActivity implements LoginContract.loginVi
         versionPresenter.updateVersion();
         mEtPassWord.setText("111111");
         mEtUserName.setText(UserInfoSingle.getInstance().getLoginName());
-        mEtUserName.setText("zhouqing");
+        mEtUserName.setText("suchao");
         mPresenter = new LoginPresenter(this);
         mBtnLogin.setOnClickListener(v -> {
             login();
