f76f265800e5e9c7bce04d544fdd35e15ae0cfc3
songweida <505416496@qq.com>
2019-06-04 14:38:27 +0800

    先不版本更新

--- app/src/main/java/qx/app/freight/qxappfreight/activity/LoginActivity.java
+++ app/src/main/java/qx/app/freight/qxappfreight/activity/LoginActivity.java
@@ -73,7 +73,7 @@ public class LoginActivity extends BaseActivity implements LoginContract.loginVi
         CustomToolbar toolbar = getToolbar();
         setToolbarShow(View.VISIBLE);
         toolbar.setMainTitle(Color.WHITE, "登录");
-        checkVersion();
+//        checkVersion();
         mEtPassWord.setText("250017");
         mEtUserName.setText(UserInfoSingle.getInstance().getLoginName());
         mEtUserName.setText("zhoumi");
