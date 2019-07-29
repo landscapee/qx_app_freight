910dbc2673a0b31611f078cd871032fb72f52d4c
pengrui <472817856@qq.com>
2019-07-29 10:56:17 +0800

    fragment 创建

---
+++ app/src/main/java/qx/app/freight/qxappfreight/fragment/CargoManifestFragment.java
@@ -0,0 +1,13 @@
+package qx.app.freight.qxappfreight.fragment;
+
+import qx.app.freight.qxappfreight.app.BaseFragment;
+
+/*****
+ * 货邮舱单页面
+ */
+
+public class CargoManifestFragment extends BaseFragment {
+
+
+
+}
---
+++ app/src/main/java/qx/app/freight/qxappfreight/fragment/LnstallationFragment.java
@@ -0,0 +1,9 @@
+package qx.app.freight.qxappfreight.fragment;
+
+import qx.app.freight.qxappfreight.app.BaseFragment;
+
+/****
+ * 装机单页面
+ */
+public class LnstallationFragment extends BaseFragment {
+}
