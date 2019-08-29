4703f0d135156fbdc0868fc76a7fdf0a589fdc06
张耀 <443231186@qq.com>
2019-08-29 16:33:50 +0800

    添加 激光扫码 标记

--- app/src/main/java/qx/app/freight/qxappfreight/utils/httpUtils/RetrofitFactory.java
+++ app/src/main/java/qx/app/freight/qxappfreight/utils/httpUtils/RetrofitFactory.java
@@ -32,8 +32,8 @@ class RetrofitFactory {
             Request authorised = originalRequest.newBuilder()
 //                    .header("token", Tools.getToken())
                     .header("Content-Type", "application/json")
-                    .header("userName", UserInfoSingle.getInstance().getUsername()!= null?UserInfoSingle.getInstance().getUsername():"")
-                    .header("userId", UserInfoSingle.getInstance().getUserId()!= null?UserInfoSingle.getInstance().getUserId():"")
+//                    .header("userName", UserInfoSingle.getInstance().getUsername()!= null?UserInfoSingle.getInstance().getUsername():"")
+//                    .header("userId", UserInfoSingle.getInstance().getUserId()!= null?UserInfoSingle.getInstance().getUserId():"")
                     .method(originalRequest.method(), originalRequest.body())
                     .build();
             return chain.proceed(authorised);
